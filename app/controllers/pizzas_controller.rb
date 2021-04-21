e931d23dd1d8b8e9f23d6aa1e4152b33ecc1cd5d
Derrick Fonseca <xracer6@hotmail.com>
2021-04-21 16:03:04 -0400

    removed comments and created Pizza class method brandOnly

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,12 +1,5 @@
 class PizzasController < ApplicationController
   def index
-    @pizzas = Pizza.all
-    @pizza_brands = []
-    
-    @pizzas.each do |pizza|
-      if !@pizza_brands.include?(pizza.brand)
-        @pizza_brands << pizza.brand
-      end
-    end
+    @pizza_brands = Pizza.brandOnly 
   end
 end
