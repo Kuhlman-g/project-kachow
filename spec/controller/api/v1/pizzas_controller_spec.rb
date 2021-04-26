db5e1d244de6a3cdff30398cea47b2e30336b899
Will Hickey <whicke01@gmail.com>
2021-04-26 19:06:15 -0400

    added controller testing for pizzas_controller

---
+++ spec/controller/api/v1/pizzas_controller_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Api::V1:: PizzasController, type: :controller do
+  let!(:brand1) { Brand.create(name: "Newman's Own") }
+
+  let!(:pizza1) { Pizza.create!(brand: brand1, product_name: "Crispy Supreme", cost: 15.00) }
+
+  describe "Get#show" do
+    it "should display all of the pizzas attributes/facts for that pizza" do
+      get :show, params: {id: pizza1.id}
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq('application/json')
+
+      expect(returned_json['pizza']['product_name']).to eq(pizza1.product_name)
+      expect(returned_json['pizza']['cost']).to eq(pizza1.cost)
+
+      expect(returned_json['brand']['name']).to eq(pizza1.brand.name)
+      expect(returned_json['brand']['id']).to eq(pizza1.brand_id)
+      expect(returned_json['brand']['id']).to eq(brand1.id)
+
+    end
+  end
+end
