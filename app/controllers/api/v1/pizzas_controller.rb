f81f3343fe1a62556285b251b007f7037b372935
Will Hickey <whicke01@gmail.com>
2021-04-28 16:48:04 -0400

    added pizza and brand serializers

--- app/controllers/api/v1/pizzas_controller.rb
+++ app/controllers/api/v1/pizzas_controller.rb
@@ -1,27 +1,8 @@
 class Api::V1::PizzasController < ApplicationController
-  protect_from_forgery unless: -> { request.format.json? }
 
   def show
     selected_pizza = Pizza.find(params[:id])
-    render json: {pizza: selected_pizza, brand: selected_pizza.brand, reviews: selected_pizza.reviews} 
+
+    render json: selected_pizza, serializer: PizzaSerializer
   end
-
-  # def create
-  #   review = Review.new(review_params)
-  #   pizza = review.pizza
-
-  #   if review.save
-  #     errors = "Review added successfully."
-  #     render json: pizza.reviews
-  #   else
-  #     render json: review.errors
-  #   end
-  # end
-
-  # private
-
-  # def review_params
-  #   params.require(:review).permit(:name, :rating, :body, :pizza)
-  # end
-  
 end
