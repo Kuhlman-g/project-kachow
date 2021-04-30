26aa3f138490013c7555d30971d7d6619afd6fe9
cjp <cjeanphilippe95@gmail.com>
2021-04-29 20:24:17 -0400

    -m "fixed merged conflicts"

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -3,9 +3,7 @@ class VotesController < ApplicationController
     def create
       vote = Vote.new(vote_params)
       if vote.save
-        #do something
       else 
-        # flash that something went wrong
       end
     end
 
