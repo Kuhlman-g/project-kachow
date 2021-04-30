7d8bc5a31d04da2358ec64aeb85c0bd66dda6123
kuhlman-g <kuhlman.gregory@gmail.com>
2021-04-30 10:21:13 -0400

    fixed merge conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-=======
->>>>>>> f82525bb4ace90eacc1e8ddcf292978f212bac75
   protect_from_forgery with: :exception
   protect_from_forgery unless: -> { request.format.json? }
 
