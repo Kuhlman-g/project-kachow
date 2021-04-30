a72c531fec50ae633c7a84d05356fd78a692c409
kuhlman-g <kuhlman.gregory@gmail.com>
2021-04-30 11:06:41 -0400

    fixed merge conflicts

--- app/serializers/pizza_serializer.rb
+++ app/serializers/pizza_serializer.rb
@@ -1,12 +1,6 @@
 class PizzaSerializer < ActiveModel::Serializer
   attributes :id, :product_name, :cost
 
-<<<<<<< HEAD
-  belongs_to :brand, serializer: BrandSerializer
-  has_many :reviews, serializer: ReviewSerializer
-end
-=======
   belongs_to :brand
   has_many :reviews
 end
->>>>>>> 4d07a13cf873a4ba9daa19273b242cbdb15c5c38
